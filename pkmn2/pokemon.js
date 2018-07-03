0dcee0c0b2039cb8476cfc1fdbb7f02ff7f0a508
neotenicgarden <34191087+neotenicgarden@users.noreply.github.com>
2018-07-03 20:13:14 +0100

    Update pokemon.js

--- pkmn2/pokemon.js
+++ pkmn2/pokemon.js
@@ -2,14 +2,16 @@ $(document ).ready(function(){
 	// Author: JLau, 2018
 	var pkmnDB = [];
 	// Get pokemon database from JSON file
-	$.getJSON( "pkmnjson.json", function( data ) {
+	$.getJSON( "pokemon.json", function( data ) {
 		for (i = 0; i < data.pokemon.length; i++) {
 			pkmnDB.push(data.pokemon[i]);
 		}
 	});
 	console.log(pkmnDB);
 	console.log(pkmnDB.length);
+	console.log(pkmnDB[0].pokemon);
 	
+	// testing
 	// $("#x-name").text(pkmnDB[0].pokemon);
 	
 	
